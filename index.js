2d94f1b98521542622078ec0ff8f9470f65de6e4
holgermueller <9jholger@gmail.com>
2024-01-05 17:01:37 -0600

    some styling

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const json = [
+  {
+    category: "Reaction",
+    score: 80,
+    icon: "./assets/images/icon-reaction.svg",
+  },
+  {
+    category: "Memory",
+    score: 92,
+    icon: "./assets/images/icon-memory.svg",
+  },
+  {
+    category: "Verbal",
+    score: 61,
+    icon: "./assets/images/icon-verbal.svg",
+  },
+  {
+    category: "Visual",
+    score: 72,
+    icon: "./assets/images/icon-visual.svg",
+  },
+];
+
+function pushJSONtoDOM() {
+  json.forEach((obj) => {
+    document.getElementsByClassName("category").innerHTML = obj.category;
+    console.log(obj.category);
+  });
+}
+
+pushJSONtoDOM();
